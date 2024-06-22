eb93c84f4a2878ab294a07e06592460a37a51016
whiterven <163173007+whiterven@users.noreply.github.com>
2024-06-22 02:11:00 +0000

    Update config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
+    PLAID_SECRET = os.getenv('PLAID_SECRET')
+    PLAID_ENV = os.getenv('PLAID_ENV')
